a6c2a5ccf798cfc9b51ce2d243a114a4503b935b
Linda Hedhli <lindahedhli818@gmail.com>
2023-03-17 19:14:48 -0700

    assign a random number to the variable n each time it is executed

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,3 +1,10 @@
 #!/bin/bash
-gcc -Wall -pedantic -Werror -Wextra -std=gnu89 0-positive_or_negative.c -o 0-positive_or_negative && ./0-positive_or_negative | awk '{print $1, "is", ($1>0)?"positive":($1<0)?"negative":"zero"}'
+n=$((RANDOM - 32767 / 2))
+if ((n > 0)); then
+    echo "$n is positive"
+elif ((n == 0)); then
+    echo "$n is zero"
+else
+    echo "$n is negative"
+fi
 
