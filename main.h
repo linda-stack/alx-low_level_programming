6ec413813a2bef092b6f5b979897ce46dfa6a640
Linda Hedhli <lindahedhli818@gmail.com>
2023-03-27 16:18:05 -0700

    change value of an integer to 98

--- main.h
+++ main.h
@@ -1,4 +1,19 @@
-#include "main.h"
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+
 
 /**
  * _isupper - checks for uppercase character
@@ -13,3 +28,4 @@ int _isupper(int c)
     else
         return (0);
 }
+
