67c2e25b379736ab77f61e68a5d30a844f51ca3f
Linda Hedhli <lindahedhli818@gmail.com>
2023-03-27 16:21:41 -0700

    change value of an integer to 98

--- main.h
+++ main.h
@@ -1,18 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
 
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
 
 
 /**
@@ -28,4 +16,5 @@ int _isupper(int c)
     else
         return (0);
 }
+
 
