1cfed4eef5429ee0cea5a6309108a5848acf5d51
Linda Hedhli <lindahedhli818@gmail.com>
2023-03-27 18:04:59 -0700

    Print half of string followed by a new line

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,6 +8,7 @@
  */
 void puts_half(char *str)
 {
+int i;
 int len = 0;
 int n;
 
@@ -25,9 +26,10 @@ else
 n = (len - 1) / 2;
 }
 
-for (int i = n; i < len; i++)
+for (i = n; i < len; i++)
 {
 putchar(str[i]);
 }
 putchar('\n');
 }
+
