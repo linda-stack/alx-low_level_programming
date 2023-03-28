cc25bc02c75e7f150f83db4b5aa0d2100be2fe03
Linda Hedhli <lindahedhli818@gmail.com>
2023-03-27 17:37:24 -0700

    Print a string in reverse followed by a line

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,19 +9,18 @@
 void print_rev(char *s)
 {
 int len = 0;
-int i;
- 
+int i; 
 while (s[len] != '\0')
 {
 len++;
 }
-
 for (i = len - 1; i >= 0; i--)
 {
 putchar(s[i]);
 }
 putchar('\n');
 }
+
 
 
 
