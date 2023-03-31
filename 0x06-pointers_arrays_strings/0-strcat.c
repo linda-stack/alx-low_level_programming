aaad6a605a8b94398ca4532d131625eb120e3723
Linda Hedhli <lindahedhli818@gmail.com>
2023-03-30 18:05:16 -0700

    concatenate two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,7 +8,7 @@
  *src string to the dest string,
  * overwriting the terminating null byte
  * (\0) at the end of dest, and then
- * adds a terminating null byte. Returns 
+ * adds a terminating null byte. Returns
  *a pointer to the resulting string dest.
  *
  * Return: pointer to the resulting string dest
@@ -17,13 +17,10 @@ char *_strcat(char *dest, char *src)
 {
 int dest_len = 0;
 int i;
-/* Find the length of the destination string */
 while (dest[dest_len] != '\0')
 dest_len++;
-/* Append the source string to the destination string */
 for (i = 0; src[i] != '\0'; i++)
 dest[dest_len + i] = src[i];
-/* Add the null terminator */
 dest[dest_len + i] = '\0';
 return (dest);
 }
