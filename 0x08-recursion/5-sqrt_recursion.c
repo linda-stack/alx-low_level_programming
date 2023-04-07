9fb2a586462ceed5807eebc5c23d691ea479f3fa
Linda Hedhli <lindahedhli818@gmail.com>
2023-04-06 20:07:04 -0700

    return natural square root of a number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -15,3 +15,16 @@ if (i * i == n)
 return (i);
 return (find_root(n, i + 1));
 }
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: the number to find the square root of
+ *
+ * Return: the natural square root of n or -1 if n does not have a natural
+ * square root
+ */
+int _sqrt_recursion(int n)
+{
+    if (n < 0)
+        return (-1);
+    return (find_root(n, 0));
+}
