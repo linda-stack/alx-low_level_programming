0448e9278402b33348476e61965efe02bce0a802
Linda Hedhli <lindahedhli818@gmail.com>
2023-04-28 08:52:19 -0700

    Print all the elements of a list_t list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,59 +11,18 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
+size_t count = 0;
 
-	while (h != NULL)
-	{
-		if (h->str == NULL)
-			printf("[0] (nil)\n");
-		else
-			printf("[%d] %s\n", h->len, h->str);
+while (h != NULL)
+{
+if (h->str == NULL)
+printf("[0] (nil)\n");
+else
+printf("[%d] %s\n", h->len, h->str);
 
-		count++;
-		h = h->next;
-	}
-
-	return (count);
+count++;
+h = h->next;
 }
 
-/**
- * main - Demonstrates the use of the print_list function
- *
- * Return: Always 0
- */
-int main(void)
-{
-	list_t *head;
-	list_t *new;
-	size_t n;
-
-	head = NULL;
-
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
-	{
-		printf("Error: memory allocation failed\n");
-		return (1);
-	}
-	new->str = strdup("Hello");
-	new->len = strlen(new->str);
-	new->next = head;
-	head = new;
-
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
-	{
-		printf("Error: memory allocation failed\n");
-		return (1);
-	}
-	new->str = strdup("World");
-	new->len = strlen(new->str);
-	new->next = head;
-	head = new;
-
-	n = print_list(head);
-	printf("The list contains %lu elements\n", n);
-
-	return (0);
+return (count);
 }
