aec9a6fcddb2ca99234491622a1fb8eabb7045d9
Linda Hedhli <lindahedhli818@gmail.com>
2023-04-28 09:52:41 -0700

    Print a message before the main function is executed

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
-
+/**
+ * premain - Prints a message before the main function is executed
+ */
 void __attribute__ ((constructor)) premain()
 {
 printf("You're beat! and yet, you must allow,\n");
