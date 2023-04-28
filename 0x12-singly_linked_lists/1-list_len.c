0de11a18a392a0c55099619421d7481c05371b3f
Linda Hedhli <lindahedhli818@gmail.com>
2023-04-28 08:11:08 -0700

    Return the number of elements in a linked list_t list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,18 +1,26 @@
 #include "lists.h"
 
 /**
- * list_len - returns the number of elements in a linked list_t list
- * @h: pointer to the head of the list
+ * list_len_recursive - Returns the number of elements in a list_t list
+ * @h: The list_t list
  *
- * Return: the number of elements
+ * Return: The number of elements in h
+ */
+size_t list_len_recursive(const list_t *h)
+{
+	if (h == NULL)
+		return (0);
+
+	return (1 + list_len_recursive(h->next));
+}
+
+/**
+ * list_len - Returns the number of elements in a list_t list
+ * @h: The list_t list
+ *
+ * Return: The number of elements in h
  */
 size_t list_len(const list_t *h)
 {
-size_t count = 0;
-while (h != NULL)
-{
-h = h->next;
-count++;
-}
-return (count);
+	return (list_len_recursive(h));
 }
