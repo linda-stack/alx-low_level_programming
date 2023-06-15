3e14e5308a97d740b3466e07a5dd302646a2a1cc
Linda Hedhli <lindahedhli818@gmail.com>
2023-06-14 17:24:51 -0700

    multiply two arguments

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,13 +1,14 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
  * main - multiplies two numbers
- * @argc: the number of command line arguments
+ * @argc: the number of command
+ *line arguments
  * @argv: an array of pointers
  *to the command line arguments
  *
- * Return: 0 if successful, 1 if the program
+ * Return: 0 if successful, 1 if
+ *the program
  *does not receive two arguments
  */
 int main(int argc, char *argv[])
@@ -16,11 +17,11 @@ int a, b, result;
 if (argc != 3)
 {
 printf("Error\n");
-return (1);
+return(1);
 }
 a = atoi(argv[1]);
 b = atoi(argv[2]);
-result = a * b;
+result = a*b;
 printf("%d\n", result);
-return (0);
+return(0);
 }
