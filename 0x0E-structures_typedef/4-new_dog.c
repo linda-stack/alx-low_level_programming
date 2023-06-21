9244929f82d5dda49c0a767ea0e7104f15ed7eea
Linda Hedhli <lindahedhli818@gmail.com>
2023-06-20 17:09:57 -0700

    free dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,34 +12,33 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *d;
-    char *name_copy;
-    char *owner_copy;
+dog_t *d;
+char *name_copy;
+char *owner_copy;
+d = malloc(sizeof(*d));
+if (d == NULL)
+return (NULL);
 
-    d = malloc(sizeof(*d));
-    if (d == NULL)
-        return (NULL);
+name_copy = malloc(strlen(name) + 1);
+if (name_copy == NULL)
+{
+free(d);
+return (NULL);
+}
+strcpy(name_copy, name);
 
-    name_copy = malloc(strlen(name) + 1);
-    if (name_copy == NULL)
-    {
-        free(d);
-        return (NULL);
-    }
-    strcpy(name_copy, name);
+owner_copy = malloc(strlen(owner) + 1);
+if (owner_copy == NULL)
+{
+free(name_copy);
+free(d);
+return (NULL);
+}
+strcpy(owner_copy, owner);
 
-    owner_copy = malloc(strlen(owner) + 1);
-    if (owner_copy == NULL)
-    {
-        free(name_copy);
-        free(d);
-        return (NULL);
-    }
-    strcpy(owner_copy, owner);
+d->name = name_copy;
+d->age = age;
+d->owner = owner_copy;
 
-    d->name = name_copy;
-    d->age = age;
-    d->owner = owner_copy;
-
-    return (d);
+return (d);
 }
