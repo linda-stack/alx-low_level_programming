635a0b720f12560345aa067001fa475da26fd02c
Linda Hedhli <lindahedhli818@gmail.com>
2023-08-30 18:55:49 -0700

    recursion project

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+* is_palindrome - returns 1 if a string is a palindrome and 0 if not
+* @s: string to check
+*
+* Return: 1 if s is a palindrome, otherwise 0
+*/
+int is_palindrome(char *s)
+{
+int len = _strlen_recursion(s);
+return _is_palindrome_helper(s, len - 1, 0);
+}
+
+/**
+* _is_palindrome_helper - helper function to check if a string is a palindrome
+* @s: string to check
+* @end: index of the last character in the string
+* @start: index of the first character in the string
+*
+* Return: 1 if s is a palindrome, otherwise 0
+*/
+int _is_palindrome_helper(char *s, int end, int start)
+{
+if (start >= end)
+{
+return 1;
+}
+if (s[start] != s[end])
+{
+return 0;
+}
+return _is_palindrome_helper(s, end - 1, start + 1);
+}
+
+/**
+* _strlen_recursion - returns the length of a string
+* @s: string to measure
+*
+* Return: length of the string
+*/
+int _strlen_recursion(char *s)
+{
+if (*s == '\0')
+{
+return 0;
+}
+return 1 + _strlen_recursion(s + 1);
+}
---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+* _sqrt_recursion - returns the natural square root of a number
+* @n: number to calculate the square root of
+*
+* Return: natural square root of n
+*/
+int _sqrt_recursion(int n)
+{
+if (n < 0)
+{
+return -1;
+}
+return _sqrt_helper(n, 0);
+}
+
+/**
+* _sqrt_helper - helper function to find the natural square root of a number
+* @n: number to calculate the square root of
+* @i: current guess for the square root
+*
+* Return: natural square root of n
+*/
+int _sqrt_helper(int n, int i)
+{
+if (i * i == n)
+{
+return i;
+}
+if (i * i > n)
+{
+return -1;
+}
+return _sqrt_helper(n, i + 1);
+}
