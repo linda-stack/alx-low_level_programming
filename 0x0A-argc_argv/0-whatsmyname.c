ad374838e5e82db7d6f3c93f2ada32f8ec311797
Linda Hedhli <lindahedhli818@gmail.com>
2023-09-01 16:08:09 -0700

    argc argv project

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,10 +8,9 @@
  *
  * Return: Always 0
  */
-int main(int argc, char *argv[])
+int main(int argc, char **argv[])
 {
-int i;
-for (i=0; i<argc, i++)
+(void)argc;
 printf("%s \n", argv[0]);
 return (0);
 }
