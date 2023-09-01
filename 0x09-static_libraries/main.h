382f81cc3ffd8f95721a85038279652baa13df02
Linda Hedhli <lindahedhli818@gmail.com>
2023-09-01 15:41:28 -0700

    static libraries project

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -149,7 +149,8 @@ char *_memcpy(char *dest, char *src, unsigned int n);
  * @s: the string
  * @c: the character to be located
  *
- * Return: a pointer to the first occurrence of the character in the string
+ * Return: a pointer to the first occurrence
+ *of the character in the string
  * or NULL if the character is not found
  */
 char *_strchr(char *s, char c);
@@ -169,19 +170,22 @@ unsigned int _strspn(char *s, char *accept);
  * @s: the string to be searched
  * @accept: the string containing the characters to search for
  *
- * Return: a pointer to the first occurrence in s of any of the characters
+ * Return: a pointer to the first occurrence in
+ *s of any of the characters
  * in accept or NULL if no such character is found
  */
 char *_strpbrk(char *s, char *accept);
 /**
- * Finds the first occurrence of the substring needle in the string haystack.
+ * Finds the first occurrence of the substring
+ *needle in the string haystack.
  *
  * Args:
  *   haystack: The string to search.
  *   needle: The substring to search for.
  *
  * Returns:
- *   The pointer to the first occurrence of needle in haystack, or NULL if needle
+ *   The pointer to the first occurrence
+ *of needle in haystack, or NULL if needle
  *   is not found.
  */
 char *_strstr(char *haystack, char *needle);
