dffdc75aaf89f93da2d51543a626bec335d156ae
Linda Hedhli <lindahedhli818@gmail.com>
2023-09-07 16:00:19 -0700

    more malloc

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -3,31 +3,16 @@
 #include <stdio.h>
 /**
 * Writes a character to stdout
-*/
-int _putchar(char c);
-/**
-* Allocates memory using malloc and exits with status 98 on failure
-*/
-void *malloc_checked(unsigned int b);
-
-/**
-* Concatenates two strings, using at most n bytes from s2
-*/
-char *string_nconcat(char *s1, char *s2, unsigned int n);
-
-/**
+*Allocates memory using malloc and exits with status 98 on failure
+*Concatenates two strings, using at most n bytes from s2
 *Allocates memory for an array, using malloc, and initializes it to zero
-*/
-void *_calloc(unsigned int nmemb, unsigned int size);
-
-/**
 *Creates an array of integers containing all numbers from min to max
-*/
-int *array_range(int min, int max);
-
-/**
 *Reallocates a memory block using malloc and free
 */
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-
 #endif
