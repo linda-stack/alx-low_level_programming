87ea91ad6ac7c0292dd21406d2b7eaee9c0bcbd7
Linda Hedhli <lindahedhli818@gmail.com>
2023-09-09 15:52:44 -0700

    malloc project

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -10,12 +10,6 @@ void *malloc_checked(unsigned int b)
 void *ptr = (malloc(b));
 if (ptr == NULL)
 {
-char *str = ("Error: malloc failed\n");
-char *c;
-for (c = str; *c != '\0'; c++)
-{
-putchar(*c);
-}
 exit(98);
 }
 return (ptr);
