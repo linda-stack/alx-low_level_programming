008862bd5fc7f401ceea7c352b5f3f772c0fc979
Linda Hedhli <lindahedhli818@gmail.com>
2023-09-09 17:28:56 -0700

    more pointers and arrays and strings

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -13,9 +13,11 @@ char *_strstr(char *haystack, char *needle)
 int i, j, k;
 for (i = 0; haystack[i] != '\0'; i++)
 {
-for (j = i, k = 0; needle[k] != '\0' && haystack[j] == needle[k]; j++, k++);
+for (j = i, k = 0; (needle[k] != '\0') && (haystack[j] == needle[k]); j++, k++);
+{
 if (needle[k] == '\0')
 return (haystack + i);
+}
 }
 return (NULL);
 }
