e3c426c17552fb57dca97e25eb5561ce511d2aef
Linda Hedhli <lindahedhli818@gmail.com>
2023-09-09 17:37:50 -0700

    project

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,4 +1,5 @@
-# include "main.h"
+#include "main.h"
+
 /**
  * _strstr - finds the first occurrence of a substring in a string
  *
@@ -13,9 +14,14 @@ char *_strstr(char *haystack, char *needle)
 int i, j, k;
 for (i = 0; haystack[i] != '\0'; i++)
 {
-for (j = i, k = 0; (needle[k] != '\0') && (haystack[j] == needle[k]); j++, k++);
+for (j = i, k = 0; (needle[k] != '\0') && (haystack[j] == needle[k]); j++, k++)
+{
+;
+}
 if (needle[k] == '\0')
+{
 return (haystack + i);
+}
 }
 return (NULL);
 }
