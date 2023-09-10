cf4eff39cc1ee5d53c7bc505bf6aa97167d942eb
Linda Hedhli <lindahedhli818@gmail.com>
2023-09-09 17:43:42 -0700

    project

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -4,7 +4,8 @@
  * @s: string to search in
  * @accept: set of bytes to search for
  *
- * Return: pointer to the byte in s that matches one of the bytes in accept, or NULL if no such byte is found.
+ * Return: pointer to the byte in s that matches one of
+ *the bytes in accept, or NULL if no such byte is found.
  */
 char *_strpbrk(char *s, char *accept)
 {
