c037ad6129a71fb76d3e90e7a24da71cbb3f8b0a
Linda Hedhli <lindahedhli818@gmail.com>
2023-09-11 14:17:19 -0700

    preprocessor project

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -2,11 +2,11 @@
 #define FUNCTION_LIKE_MACRO_H
 
 /**
- * File: 3-function_like_macro.h
- * Auth: (your name here)
- * Desc: Header file that defines a function-like macro ABS(x) that
- *       computes the absolute value of a number x.
- */
+* File: 3-function_like_macro.h
+* Auth: (your name here)
+* Desc: Header file that defines a function-like macro ABS(x) that
+*computes the absolute value of a number x.
+*/
 
 #include <stdio.h>
 #include <stdlib.h>
--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -2,11 +2,11 @@
 #define SUM_H
 
 /**
- * File: 4-sum.h
- * Auth: (your name here)
- * Desc: Header file that defines a function-like macro SUM(x, y) that
- * computes the sum of the numbers x and y.
- */
+* File: 4-sum.h
+* Auth: (your name here)
+* Desc: Header file that defines a function-like macro SUM(x, y) that
+* computes the sum of the numbers x and y.
+*/
 
 #define SUM(x, y) ((x) + (y))
 #endif
