ca2441af50a38d30130907787f28908c63a52f8c
Linda Hedhli <lindahedhli818@gmail.com>
2023-09-11 13:11:32 -0700

    preprocessor project

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -8,5 +8,46 @@
  *       computes the absolute value of a number x.
  */
 
-#define ABS(x) ((x) < 0 ? -(x) : (x))
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ABS(x) _Generic((x), \
+    long long: llabs, \
+    default: labs, \
+    float: fabsf, \
+    double: fabs, \
+    long double: fabsl)(x)
+
+int _putchar(char c)
+{
+return (write(1, &c, 1));
+}
+
+void print_number(int n)
+{
+if (n < 0)
+{
+_putchar('-');
+n = -n;
+}
+if (n / 10)
+print_number(n / 10);
+_putchar(n % 10 + '0');
+}
+
+int main(void)
+{
+int i;
+int j;
+
+i = ABS(-98) * 10;
+j = ABS(98) * 10;
+print_number(i);
+_putchar(',');
+_putchar(' ');
+print_number(j);
+_putchar('\n');
+
+return (EXIT_SUCCESS);
+}
 #endif
