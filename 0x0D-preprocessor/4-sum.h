fddc831c69662cb049c4e9ad27f4404b13fe295d
linda-stack <30959659+linda-stack@users.noreply.github.com>
2023-09-13 03:48:08 +0100

    Update 4-sum.h

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -6,6 +6,8 @@
 * Auth: (your name here)
 * Desc: Header file that defines a function-like macro SUM(x, y) that
 * computes the sum of the numbers x and y.
+* @x: The first number to add.
+* @y: The second number to add.
 */
 
 #define SUM(x, y) ((x) + (y))
