c0cd3f67e2b6505491f55b5f0b77a5839cecccdc
Linda Hedhli <lindahedhli818@gmail.com>
2023-10-06 19:49:00 -0700

    project

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -20,11 +20,11 @@ ssize_t read_count, write_count;
 char *buffer;
 
 if (filename == NULL)
-    return (0);
+return (0);
 
 fd = open(filename, O_RDONLY);
 if (fd == -1)
-    return (0);
+return (0);
 
 buffer = malloc(sizeof(char) * letters);
 if (buffer == NULL)
